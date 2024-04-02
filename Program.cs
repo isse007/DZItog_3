7735b66d04b59b5a0f98636d8c0cca04ab354e4c
Simon Isupov <is_se@mail.ru>
2024-04-02 23:28:07 +0300

    3

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 3: Задайте произвольный массив. 
+// Выведете его элементы, начиная с конца. 
+// Использовать рекурсию, не использовать циклы.
+
+
+void PrintBackArray (int[] arr, int i)
+{
+    if (i<0)
+    {
+        return;
+    }
+    System.Console.WriteLine(arr[i]);
+    PrintBackArray(arr, i-1);
+}
+int[] array = {5,7,9};
+PrintBackArray (array,array.Length-1);
+
